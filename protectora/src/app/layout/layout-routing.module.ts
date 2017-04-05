341cfd393bdc0e243c41585fcabdba063ea44761
Formacion815 <Formacion815@stkproj.com>
2017-04-05 14:20:47 +0200

    Adding base securitation

--- protectora/src/app/layout/layout-routing.module.ts
+++ protectora/src/app/layout/layout-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from './../seguridad/seguridad.service'
 
 const routes: Routes = [
   { path: '', loadChildren: './../home/home.module#HomeModule' },
-  { path: 'animales', loadChildren: './../animales/animales.module#AnimalesModule' },
+  { path: 'animales', loadChildren: './../animales/animales.module#AnimalesModule', canActivate: [AuthGuard],
+ },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthGuard]
 })
 export class LayoutRoutingModule { }
