5c2cc0e001e7b3b46864ddaee66dfb20facae955
Formacion816 <Formacion816@stkproj.com>
2017-04-05 12:57:44 +0200

    add nuew animals

--- protectora/src/app/animales/animales.module.ts
+++ protectora/src/app/animales/animales.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NuevoComponent } from './nuevo/nuevo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NuevoComponent]
 })
 export class AnimalesModule { }
