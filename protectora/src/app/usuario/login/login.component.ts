985a60c8fcad24e98bf191e5fd358f74409bda3e
Formacion818 <Formacion818@stkproj.com>
2017-04-06 10:27:00 +0200

    Actualizado login

--- protectora/src/app/usuario/login/login.component.ts
+++ protectora/src/app/usuario/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsuarioService } from './../usuario.service';
+
 @Component({
-  selector: 'smn-login',
+  selector: 'cf-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+/**
+ * Ask user for credentials
+ * Send to a service and logs in the user
+ * */
 export class LoginComponent implements OnInit {
+  /**
+   * Some questions to indentify the user
+   */
+  private credentials = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
   }
+  /**
+   * the user clicks the register in button
+   */
+  alRegistrar() {
+    this.usuarioService.postUser$(this.credentials).subscribe();
+  }
+  /**
+   * the user clicks the log in button
+   */
+  alEntrar() {
+    this.usuarioService.postSesion$(this.credentials).subscribe();
+  }
 
+  showError(control) {
+    return control.errors && (control.dirty || control.touched)
+  }
 }
--- protectora/src/app/usuario/usuario.service.ts
+++ protectora/src/app/usuario/usuario.service.ts
@@ -1,20 +1,54 @@
-import { Injectable } from '@angular/core';
-import { DatosUsuario } from './../shared/Model/model';
-import { UsuarioStoreService } from './../shared/usuario-store.service';
-import { Router } from '@angular/router';
-import { Http } from '@angular/http';
 import { CrudService } from './../shared/crud.service';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatosUsuario } from './../shared/model/model';
+import { UsuarioStoreService } from './../shared/usuario-store.service';
+
+import 'rxjs/operators/map';
 
 @Injectable()
-export class UsuarioService extends CrudService {
+/**
+ * CRUD and custom logic related to users
+ * */
+export class UserService extends CrudService {
 
   constructor(private usuarioStoreService: UsuarioStoreService, http: Http, private router: Router) {
     super(http);
     this.apiEndPoint = 'pub/usuarios';
   }
 
-
-  public getProfile(): DatosUsuario {
-    return this.usuarioStoreService.getProfile();
+  /**
+   * Sends credentials to de API and stores the result
+   * */
+  public postSesion$(credenciales) {
+    return this.http
+      .post('pub/sesiones', credenciales)
+      .map(r => {
+        const token = r.json();
+        this.usuarioStoreService.logIn({email: credenciales.email }, token);
+        this.router.navigate(['']);
+      });
   }
+
+  /**
+   * Sends credentials to de API and stores the result
+   * */
+  public postUser$(credenciales) {
+    return this.http
+      .post('pub/usuarios', credenciales)
+      .map(r => {
+        const token = r.json();
+        this.userStoreService.logIn({email: credenciales.email }, token);
+        this.router.navigate(['']);
+      });
+  }
+
+  /**
+   * Refresh the current user profile
+   * */
+  public getProfile(): UserData {
+    return this.userStoreService.getProfile();
+  }
+
 }
