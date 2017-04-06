c8bcfc5d67009068bd1d90a7c515cca6f0aeb689
Formacion816 <Formacion816@stkproj.com>
2017-04-06 10:56:05 +0200

    fix

--- protectora/src/app/animales/animales.module.ts
+++ protectora/src/app/animales/animales.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AnimalesRoutingModule } from './animales-routing.module';
 import {NuevoComponent} from './nuevo/nuevo.component'
 
@@ -8,6 +8,7 @@ import {NuevoComponent} from './nuevo/nuevo.component'
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AnimalesRoutingModule
   ],
   declarations: [NuevoComponent]
