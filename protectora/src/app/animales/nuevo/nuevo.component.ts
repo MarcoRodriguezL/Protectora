c27b7908e5337246c6c99314b7115f9b736d92e2
Formacion816 <Formacion816@stkproj.com>
2017-04-06 10:48:08 +0200

    try

--- protectora/src/app/animales/nuevo/nuevo.component.ts
+++ protectora/src/app/animales/nuevo/nuevo.component.ts
@@ -13,3 +13,34 @@ export class NuevoComponent implements OnInit {
   }
 
 }
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Tipo } from './../../shared/Model/model';
+import { Animal } from './../../shared/Model/model';
+
+@Component({
+  selector: 'smn-nuevo',
+  templateUrl: './nuevo.component.html',
+  styleUrls: ['./nuevo.component.css']
+})
+export class NuevoComponent implements OnInit {
+
+  private nuevoAnimal: FormGroup;
+  tipos: Tipo[];
+  animal: Animal;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.nuevoAnimal =this.formBuilder.group({
+      tipo: this.animal.tipo,
+      raza: this.animal.raza,
+      nombre: this.animal.nombre,
+      sexo: this.animal.sexo,
+      chip: this.animal.chip
+    });
+  }
+
+  
+
+}
